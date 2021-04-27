3f0724f995608b47b9cb13ca3f263fb68c316a29
Lars Pickelin <lars.pickelin2@vaillant-group.com>
2021-04-27 10:45:02 +0200

    finalize mapper

--- src/main/java/com/oev/apis/grosspension/service/GrossPensionMapper.java
+++ src/main/java/com/oev/apis/grosspension/service/GrossPensionMapper.java
@@ -2,14 +2,14 @@ package com.oev.apis.grosspension.service;
 
 import com.oev.apis.grosspension.model.GrossPensionRequest;
 import com.oev.apis.grosspension.model.GrossPensionResponse;
+import lombok.NonNull;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
 @Component
-// todo: add unit tests
 public class GrossPensionMapper {
-    public GrossPensionResponse map(GrossPensionRequest grossPensionRequest, BigDecimal grossPension) {
+    public GrossPensionResponse map(@NonNull GrossPensionRequest grossPensionRequest, @NonNull BigDecimal grossPension) {
         return GrossPensionResponse.builder()
                 .age(grossPensionRequest.getAge())
                 .grossAnnualSalary(grossPensionRequest.getGrossAnnualSalary())
---
+++ src/test/groovy/com/oev/apis/grosspension/service/GrossPensionMapperSpec.groovy
@@ -0,0 +1,47 @@
+package com.oev.apis.grosspension.service
+
+import com.oev.apis.grosspension.model.GrossPensionRequest
+import spock.lang.Specification
+import spock.lang.Subject
+import spock.lang.Unroll
+
+import java.time.LocalDate
+
+class GrossPensionMapperSpec extends Specification {
+
+    @Subject
+    GrossPensionMapper grossPensionMapper = new GrossPensionMapper()
+
+    @Unroll
+    def "it should #description"() {
+
+        given: "Validation indicator"
+        boolean isValid
+
+        when: "we check the postal code against the respective country regEx"
+        try {
+            grossPensionMapper.map(request, grossPension)
+            isValid = true
+        } catch (NullPointerException ignored) {
+            isValid = false
+        }
+
+        then: "we compare the result of the apply with our expectation"
+        expectedResult == isValid
+
+        where:
+        description                            | request           | grossPension || expectedResult
+        "fail if request is null"                 | null              | 30000.0      || false
+        "fail if grossPension is null"            | getValidRequest() | null         || false
+        "map successful if payload is correct" | getValidRequest() | 30000.0         || true
+
+    }
+
+    private static GrossPensionRequest getValidRequest() {
+        return GrossPensionRequest.builder()
+                .age(18)
+                .grossAnnualSalary(30000.0)
+                .startOfEmployment(LocalDate.now())
+                .build();
+    }
+}
